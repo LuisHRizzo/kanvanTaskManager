d0697aace5a5712cc789caa7c08d2bdf523b89ae
LuisHRizzo <70038673+LuisHRizzo@users.noreply.github.com>
2026-03-12 20:57:43 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    // AGREGADO: Lista de dominios permitidos
+    allowedHosts: [
+      'taskmanager.163.176.208.127.sslip.io'
+    ],
     watch: {
       usePolling: true
     }
